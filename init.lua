817d7fcac913abaaf4223a9be934cf35b9ad81f8
samisdat <bastian@pertz.eu>
2022-12-05 11:40:15 +0100

    It working so far

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("samisdat.plugins-setup")
+
+require('samisdat.core.options')
+require('samisdat.core.keymaps')
+require('samisdat.core.colorscheme')
---
+++ lua/samisdat/core/keymaps.lua
@@ -0,0 +1,21 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
