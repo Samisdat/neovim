d9828ec906955815176062728558df31dd23b57c
samisdat <bastian@pertz.eu>
2022-12-20 00:34:32 +0100

    turn on termguicolors for nightfly colorscheme

---
+++ lua/samisdat/set.lua
@@ -0,0 +1,5 @@
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+vim.opt.termguicolors = true
+vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
