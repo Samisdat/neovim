4f3d6a025226d92b5a223c131b04a3cfccc6aa12
samisdat <bastian@pertz.eu>
2022-12-19 12:46:29 +0100

    split remaps into different files while i took the from different sources

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+# Two sources
+
+ThePrimeagen
+https://www.youtube.com/watch?v=w7i4amO_zaE
+https://github.com/ThePrimeagen/init.lua
+
+joseanmartinez
+https://www.youtube.com/watch?v=vdn_pKJUda8
+https://github.com/josean-dev/dev-environment-files
+
+primeagen uses in his examples the after dir to configure plugins
+and jose uses "safe lua requires"
+
+No idea what is better, I will use jose's method for now
+
 # Look up
 
 Register in VIM
--- lua/samisdat/init.lua
+++ lua/samisdat/init.lua
@@ -1,13 +1,24 @@
 require("samisdat.plugins-setup")
 
 require("samisdat.core.options")
-require("samisdat.core.keymaps")
+require("samisdat.core.remap")
+require("samisdat.core.remap-theprimagen")
+require("samisdat.core.remap-joseanmartinez")
+
 require("samisdat.core.colorscheme")
+
 require("samisdat.plugins.comment")
+
 require("samisdat.plugins.nvim-tree")
+
 require("samisdat.plugins.lualine")
+
 require("samisdat.plugins.telescope")
+
 require("samisdat.plugins.nvim-cmp")
+
 require("samisdat.plugins.autopairs")
+
 require("samisdat.plugins.treesitter")
+
 require("samisdat.plugins.gitsigns")
