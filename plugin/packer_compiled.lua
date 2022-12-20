145d6e77a8f2593dd7a29f99604c4c55d2be246c
samisdat <bastian@pertz.eu>
2022-12-20 22:51:23 +0100

    update

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -211,8 +211,8 @@ time([[Conditional loading of telescope-fzf-native.nvim]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd nvim-treesitter ]]
-vim.cmd [[ packadd nvim-ts-autotag ]]
 vim.cmd [[ packadd nvim-treesitter-textobjects ]]
+vim.cmd [[ packadd nvim-ts-autotag ]]
 time([[Sequenced loading]], false)
 
 _G._packer.inside_compile = false
