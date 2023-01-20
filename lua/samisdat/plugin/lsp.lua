e26ebe0be4f1da1ce4317a68738d810f6b35a762
samisdat <bastian@pertz.eu>
2023-01-20 22:17:31 +0100

    update keybinding

--- lua/samisdat/plugin/lsp.lua
+++ lua/samisdat/plugin/lsp.lua
@@ -21,8 +21,8 @@ local on_attach = function(_, bufnr)
 		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
 	end
 
-	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+	nmap("<leader>n", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>a", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
 	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
